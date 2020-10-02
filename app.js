6bea34b4717aefc9adaf3dd0f9630e48e6c71686
Chris <chris.shim1999@hotmail.com>
2020-10-01 20:17:47 -0400

    Created a post endpoint to view and create posts.

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
 var loginRouter = require('./routes/login');
+var postsRouter = require('./routes/posts');
 
 const mongoDb = process.env.MONGO_URI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -83,6 +84,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/posts.js
+++ routes/posts.js
@@ -1,20 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var UserModel = require('../models/user.js');
 var passport = require('passport');
 var PostModel = require('../models/post.js');
 var myPassport = require('../config/passport')(passport);
 
 /* GET users listing. */
 router.get('/', passport.authenticate('jwt', {session: false}), function(req, res, next) {
-  UserModel.find({}, (err, docs) => {
+  PostModel.find({}, (err, docs) => {
     res.json(docs);
   });
 });
 
 router.get('/:username', passport.authenticate('jwt', {session: false}), function(req, res, next) {
-  UserModel.find({username: `${req.params.username}`}, (err, docs) => {
+    PostModel.find({username: `${req.params.username}`}, (err, docs) => {
     res.json(docs);
+  });
+});
+
+router.post('/create', passport.authenticate('jwt', {session: false}), function(req, res, next) {
+  const post = new PostModel({
+    username: req.body.username,
+    text: req.body.text,
+  });
+  
+  post.save(err => {
+    if (err) {
+      return next(err);
+    };
+    res.redirect('/');
   });
 });
 
